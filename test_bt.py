97271f9214b4e52042ccb0b8af1d79e8b297aae8
huangyueshiqi <huangyueshiqi@users.noreply.github.com>
2026-04-22 06:27:30 +0000

    Revert commits 311bfeb..311bfeb
    This reverts 1 commit(s) from 311bfeb to 311bfeb.
    
    Reverted commits:
    311bfeb feat: 生成项目Code Wiki文档
    
    Co-authored-by: traeagent <traeagent@users.noreply.github.com>

--- test_bt.py
+++
@@ -1,7 +0,0 @@
-import backtrader as bt
-
-class MyStrategy(bt.Strategy):
-    def next(self):
-        print(type(self.datetime.date(0)), self.datetime.date(0))
-
-print(hasattr(bt.linebuffer.LineBuffer, 'date'))
